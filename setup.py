04d952eeca53d72a4506a8de70bdc43b79d387ea
Chris Malone <chris.m.malone@gmail.com>
2014-10-27 14:43:16 -0600

    start on setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='brulilo',
+      version='0.1',
+      description='Toy nuclear reaction network scaffolding.',
+      url='http://bitbucket.org/ChrisMalone/brulilo',
+      author='Chris Malone',
+      author_email='chris.m.malone@gmail.com',
+      packages=['brulilo'],
+      license='TBD')

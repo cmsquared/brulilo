04d8acb35cac77d7929d35d5bb3fecd168930df6
Chris Malone <chris.m.malone@gmail.com>
2014-10-30 16:58:30 -0600

    test the reaclib interface

---
+++ brulilo/test/test_reaclib.py
@@ -0,0 +1,12 @@
+import brulilo
+from brulilo import Network
+import os.path
+
+# this is a weak connection now, should probably generalize
+rxn_file = os.path.join(os.path.dirname(brulilo.__file__),
+                        'test/testRxns.txt')
+
+net = Network.from_rxn_file(rxn_file)
+net.plot()
+
+net.pprint()

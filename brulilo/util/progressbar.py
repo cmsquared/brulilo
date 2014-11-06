dc8f3cbed87564ee3047fb31ebb5bce93fc3cd60
Chris Malone <chris.m.malone@gmail.com>
2014-11-06 11:06:22 -0700

    more informative

--- brulilo/util/progressbar.py
+++ brulilo/util/progressbar.py
@@ -8,23 +8,26 @@ class ProgressBar(object):
     _bar_width = 60
     _p_symbol = "#"
     _r_symbol = "-"
+    _done_status = "Done!\n"
 
     def __init__(self, label):
         self.label = label
         self.state = 0
         self._update_display()
 
-    def update(self):
+    def update(self, status=''):
         self.state += self.delta_update
-        self._update_display()
+        self._update_display(status)
 
-    def _update_display(self):
+    def _update_display(self, status=''):
         progress = int(self._bar_width*self.state)
         remaining = self._bar_width - progress
-        display = "\r{0}: [{1}] {2}%".format(self.label,
-                                             (self._p_symbol*progress +
-                                              self._r_symbol*remaining),
-                                             self.state*100)
+        if self.state == 1.0:
+            status = self._done_status
+        display = "\r{0}: [{1}] {2}".format(self.label,
+                                            (self._p_symbol*progress +
+                                             self._r_symbol*remaining),
+                                            status)
         sys.stdout.write(display)
         sys.stdout.flush()
 
